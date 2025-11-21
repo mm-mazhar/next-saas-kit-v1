d3cb628285942cba50f411100031e7c4a917f836
Mazhar <mazqoty.01@gmail.com>
2025-11-21 22:08:04 +0300

    CheckPoint-6: Updated About page

--- app/(marketing)/about/page.tsx
+++ app/(marketing)/about/page.tsx
@@ -4,50 +4,36 @@ import { PageSection } from '@/components/page-section'
 
 export default function AboutPage() {
   return (
-    <PageSection className='py-16'>
-      {/* The "max-w-3xl" and "mx-auto" classes have been removed from this div */}
-      <div className='space-y-8 px-4'>
-        <h1 className='text-3xl font-bold'>About</h1>
-        <p className='text-muted-foreground'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. We build
-          tools to launch SaaS apps faster.
-        </p>
-        <div className='space-y-4 text-sm text-muted-foreground'>
-          <p>
-            Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
-            Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-          </p>
-          <p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p>
-          <p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p>
-          <p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p>
-          <p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p>
-          <p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p>
-          <p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p><p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p>
-          <p>
-            Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-            semper porta.
-          </p>
+    <PageSection>
+      <div className='px-4'>
+        <div className='flex flex-col items-center justify-center gap-10 md:flex-row'>
+          <div className='relative rounded-2xl overflow-hidden shadow-2xl shadow-primary/40 shrink-0'>
+            <img
+              className='max-w-md w-full object-cover rounded-2xl'
+              src='https://images.unsplash.com/photo-1531497865144-0464ef8fb9a9?q=80&w=800&auto=format&fit=crop'
+              alt='Team'
+            />
+            <div className='flex items-center gap-2 max-w-72 absolute bottom-8 left-8 bg-background p-4 rounded-xl'>
+              <div className='flex -space-x-4 shrink-0'>
+                <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200' alt='' className='size-9 rounded-full border-[3px] border-background' />
+                <img src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200' alt='' className='size-9 rounded-full border-[3px] border-background' />
+                <img src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=200&h=200&auto=format&fit=crop' alt='' className='size-9 rounded-full border-[3px] border-background' />
+                <div className='flex items-center justify-center text-xs text-primary-foreground size-9 rounded-full border-[3px] border-background bg-primary'>
+                  50+
+                </div>
+              </div>
+              <p className='text-sm font-medium text-foreground'>Join our community</p>
+            </div>
+          </div>
+
+          <div className='text-sm text-muted-foreground max-w-lg'>
+            <h1 className='text-xl uppercase font-semibold text-foreground'>What we do?</h1>
+            <div className='w-24 h-[3px] rounded-full bg-gradient-to-r from-primary to-primary/30'></div>
+            <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, iste obcaecati. Nemo sint deleniti a porro eos enim! A, doloremque?</p>
+            <p className='mt-4'>Lorem ipsum dolor sit.0</p>
+            <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam porro modi, officiis quia inventore blanditiis veritatis error quisquam eius, praesentium magnam recusandae minus ducimus impedit quaerat cumque laborum tenetur velit!</p>
+            <p className='mt-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam iste perspiciatis reiciendis illum tenetur facilis, tempora sed ipsa explicabo nemo debitis quidem illo commodi earum porro eius maiores, dolorum ullam odit? Obcaecati accusantium neque quidem placeat? Unde rerum vel, nobis velit ducimus pariatur maxime, nemo minus tenetur saepe rem blanditiis nulla est aliquam sequi voluptates laborum quia temporibus consectetur eligendi doloribus laudantium cumque impedit? Optio rerum quaerat perferendis voluptatum, id pariatur quia veritatis architecto minima vero excepturi fuga doloribus, cum quo laborum eum, quibusdam unde porro deserunt voluptas tenetur consequuntur libero. Aliquam sed expedita deserunt atque aperiam ad laudantium amet, ullam nam alias omnis magnam nemo a eos rerum. Animi, ipsa. Sed tempore eum, tenetur odio voluptatibus debitis libero quas alias. Tempore tenetur ipsa neque nobis perspiciatis, delectus consectetur unde quas nostrum dolorum rem consequuntur esse alias, minima distinctio, molestias voluptate deserunt odit! Enim officiis voluptates at ea reiciendis. Vel hic molestias consequuntur eos dignissimos labore architecto accusamus nemo! Dolorum aspernatur tempore esse assumenda earum perferendis reprehenderit? Hic enim vero exercitationem asperiores dolor dolores, rem nostrum est distinctio voluptas consequatur repellendus beatae officiis fugiat corrupti voluptate deleniti minus. Voluptate unde id excepturi. Minima velit fugit eveniet ea blanditiis animi maiores.</p>
+          </div>
         </div>
       </div>
     </PageSection>
